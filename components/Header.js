3ad405505995da876bb4939c03b53e55d144308d
Erkhembayar Atarbold <74484951+TheOverLordEA@users.noreply.github.com>
2021-07-31 01:04:39 -0700

    the top component built

--- components/Header.js
+++ components/Header.js
@@ -1,34 +1,32 @@
 import {
-    BadgeCheckIcon,
-    CollectionIcon,
-    HomeIcon,
-    LightningBoltIcon,
-    SearchIcon,
-    UserIcon,
-} from '@heroicons/react/outline'
+  BadgeCheckIcon,
+  CollectionIcon,
+  HomeIcon,
+  LightningBoltIcon,
+  SearchIcon,
+  UserIcon,
+} from "@heroicons/react/outline";
 
-
-
-import Image from 'next/image'
-import EBrand from '../public/assets/images/GreenCartIcon.svg'
-import HeaderItem from './HeaderItem'
+import Image from "next/image";
+import EBrand from "../public/assets/images/GreenCartIcon.svg";
+import HeaderItem from "./HeaderItem";
 
 const Header = () => {
-    return (
-        <header className=''>
-        
-            <Image 
-            
-            className='object-contain'
-            src={EBrand} alt='a image'
-            width={200}
-            height={100}
-            />
-            <div>
-                <HeaderItem title='HOME' Icon={HomeIcon}/>
-            </div>
-        </header>
-    )
-}
+  return (
+    <header className="">
+      <Image
+        className="object-contain"
+        src={EBrand}
+        alt="a image"
+        width={200}
+        height={100}
+      />
+      <div>
+        <HeaderItem title="HOME" Icon={HomeIcon} />
+        <HeaderItem title="" Icon={HomeIcon} />
+      </div>
+    </header>
+  );
+};
 
-export default Header
+export default Header;
