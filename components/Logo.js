f596a7002cb3958c183f7313fc7537b8deeea400
Erkhembayar Atarbold <74484951+TheOverLordEA@users.noreply.github.com>
2021-08-04 05:55:04 -0700

    the header  image changed

--- components/Logo.js
+++ components/Logo.js
@@ -1,5 +1,17 @@
+import Image from "next/image";
+
 const Logo = () => {
-  return <div className></div>;
+  return (
+    <div className="flex items-center bg-gray-500 max-w-6x1 h-1">
+      <Image
+        className="object-contain"
+        src={EBrand}
+        alt="a image"
+        width={180}
+        height={100}
+      />
+    </div>
+  );
 };
 
 export default Logo;
