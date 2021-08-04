2c60726d282dac71821f6e6a3bb5cb62c2deee7b
Erkhembayar Atarbold <74484951+TheOverLordEA@users.noreply.github.com>
2021-08-04 06:15:02 -0700

    the menu created

--- components/Header.js
+++ components/Header.js
@@ -6,20 +6,12 @@ import {
 } from "@heroicons/react/outline";
 
 import Image from "next/image";
-import EBrand from "../public/assets/images/GreenCartIcon.svg";
 import HeaderItem from "./HeaderItem";
 import Search from "./Search";
 
 const Header = () => {
   return (
     <header className="flex items-center bg-black">
-      <Image
-        className="object-contain"
-        src={EBrand}
-        alt="a image"
-        width={180}
-        height={100}
-      />
       <Search className="flex flex: 1 1 0% items-center" />
       <div className="flex items-center">
         <HeaderItem title="HOME" Icon={HomeIcon} />
--- components/Logo.js
+++ components/Logo.js
@@ -1,15 +1,9 @@
-import Image from "next/image";
-
 const Logo = () => {
   return (
-    <div className="flex items-center bg-gray-500 max-w-6x1 h-1">
-      <Image
-        className="object-contain"
-        src={EBrand}
-        alt="a image"
-        width={180}
-        height={100}
-      />
+    <div className="flex bg-gray-500 max-w-6x1 h-3em">
+      <div className="flex items-center">
+        <h1>The Logo</h1>
+      </div>
     </div>
   );
 };
