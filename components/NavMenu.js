2ef1528a266ae2e54c51c27fda65fe2a27a85641
Erkhembayar Atarbold <74484951+TheOverLordEA@users.noreply.github.com>
2021-08-05 07:29:22 -0700

    web burger component altered

--- components/NavMenu.js
+++ components/NavMenu.js
@@ -1,85 +1,39 @@
-import React from "react";
-
 import { slide as Menu } from "react-burger-menu";
 import Link from "next/link";
+import React from "react";
 
-class Sidebar extends React.Component {
-  state = {
-    menuOpen: false,
-  };
-
+class NavMenu extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      menuOpen: false,
+    };
+  }
   handleStateChange(state) {
     this.setState({ menuOpen: state.isOpen });
   }
-
   closeMenu() {
     this.setState({ menuOpen: false });
   }
-
   render() {
     return (
       <Menu
+        noOverlay
         width={180}
-        className="flex outline-none top-0 "
         isOpen={this.state.menuOpen}
         onStateChange={(state) => this.handleStateChange(state)}
       >
-        <Link
-          href="About"
-          className="menu-item"
-          to="About"
-          smooth={true}
-          duration={500}
-        >
-          About
+        <Link href="/Build">
+          <div onClick={() => this.closeMenu()}>Build</div>
         </Link>
-        <Link
-          href="/menu-item"
-          className="menu-item"
-          to="TopCategories"
-          smooth={true}
-          duration={500}
-        >
-          Top-level Categories
+        <Link href="/Build">
+          <div onClick={() => this.closeMenu()}>Build</div>
         </Link>
-        <Link
-          href="Subcategories"
-          className="menu-item"
-          to="SubCategories"
-          smooth={true}
-          duration={500}
-        >
-          Subcategories
-        </Link>
-        <Link
-          href="ee"
-          className="menu-item"
-          to="EmbededPlatforms"
-          smooth={true}
-          duration={500}
-        >
-          Embedded Platforms
-        </Link>
-        <Link
-          href="top"
-          className="menu-item"
-          to="Users"
-          smooth={true}
-          duration={500}
-        >
-          Top Users
-        </Link>
-        <Link
-          href="top"
-          className="menu-item"
-          to="Sentiments"
-          smooth={true}
-          duration={500}
-        >
-          Sentiments
+        <Link href="/Build">
+          <div onClick={() => this.closeMenu()}>Build</div>
         </Link>
       </Menu>
     );
   }
 }
-export default Sidebar;
+export default NavMenu;
