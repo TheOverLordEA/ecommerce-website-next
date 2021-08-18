0d4497f598ce95ef99abb5bf1b3878bb359b9d9e
Erkhembayar Atarbold <74484951+TheOverLordEA@users.noreply.github.com>
2021-08-17 22:00:57 -0700

    the menu added

--- components/Header.js
+++ components/Header.js
@@ -10,12 +10,12 @@ import HeaderItem from "./HeaderItem";
 
 const Header = () => {
   return (
-    <div className="sticky">
-      <header className="flex items-center bg-black w-auto p-2">
+    <header>
+      <div className="flex items-center bg-black w-100% p-3 ">
         <NavMenu />
 
         <Search className="flex flex: 1 1 0% items-center rounded" />
-        <div className="flex items-center p-1 w-18 ">
+        <div className="flex items-center ">
           <div className=" ">
             <HeaderItem title="Where" Icon={LocationMarkerIcon} />
           </div>
@@ -25,13 +25,24 @@ const Header = () => {
           <div className="">
             <HeaderItem title="HOME" Icon={UserIcon} />
           </div>
-
-          <div className="">
-            <HeaderItem title="USER" Icon={ShoppingCartIcon} />
+          <div className="flex items-center">
+            <span className="absolute right-4 top-10 h-6 w-6 bg-green-800 text-center text-white rounded-full font-bold">
+              {" "}
+              8
+            </span>
+            <HeaderItem title="BASKET" Icon={ShoppingCartIcon} />
           </div>
         </div>
-      </header>
-    </div>
+      </div>
+
+      <div className="flex items-center bg-yellow-500 text-white space-x-3 p-2 pl-6 text-base ">
+        <p className="link items-center">8</p>
+        <p className="link">Prime</p>
+        <p className="">Best</p>
+        <p className="">New</p>
+        <p className="">World Market</p>
+      </div>
+    </header>
   );
 };
 
--- components/HeaderItem.js
+++ components/HeaderItem.js
@@ -2,7 +2,7 @@ const HeaderItem = ({ Icon, title }) => {
   return (
     <div className="flex flex-col items-center cursor-pointer group w-12 sm:w-20 hover:text-white link">
       <Icon className="h-8 mb-1 group-hover:animate-bounce" />
-      <p className="opacity-0 group-hover:opacity-100 tracking-widest top-8 hidden">
+      <p className="opacity-0 group-hover:opacity-100 tracking-widest top-8 hidden font-extrabold md:text-xs">
         {title}
       </p>
     </div>
